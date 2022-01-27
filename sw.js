f5eac0486b0ed5416a5a3c7885251979845963f4
infinite-war <a969557028@gmail.com>
2022-01-27 09:29:55 +0800

    Site updated: 2022-01-27 09:29:54

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('active', function () {
 });
 
 var precacheUrls = [
-'/','/posts/16107/',
+'/','/posts/0/','/posts/16107/',
 ];
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
